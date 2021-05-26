df840dc190c5a5d8a3bfde6e8088099df94c9c2b
Leo <leodrl9@gmail.com>
2021-05-26 00:23:20 -0300

    add todos

--- app/stock/views.py
+++ app/stock/views.py
@@ -5,6 +5,17 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from stock.forms import LoginForm
 
+# TODO: create the chat room
+# - create a view that shows the 50 messages from the model model timestamp, username and messages 
+# - create a template with a char field to be posted
+# - in the post get the user data and write in model timestamp, username and messages
+
+# TODO: create a chat bot using a celery as a cron 1min with rabitmq
+# bot:
+# - get data from "https://stooq.com/q/l/?s=aapl.us&f=sd2t2ohlcv&h&e=csv"
+# - parse the csv with ","
+# - send it for the model(timestamp, username and messages) with the messages time to time(1min)
+
 # implement this others latter
 # TODO: START implement this later
 def logged(request):
